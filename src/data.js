ed8e534ef0a082a8c709f1503b30b6ff8b9dd58d
IndraDevlop <indrawansyah2109@gmail.com>
2025-12-20 06:02:11 +0700

    fix: remove all asset imports and use static strings

--- src/data.js
+++ src/data.js
@@ -113,18 +113,12 @@ export const listTools = [
   
 ];
 
-import Proyek1 from "./assets/proyek/proyek1.webp";
-import Proyek2 from "./assets/proyek/proyek2.webp";
-import Proyek3 from "./assets/proyek/proyek3.webp";
-import Proyek4 from "./assets/proyek/proyek4.webp";
-import Proyek5 from "./assets/proyek/proyek5.webp";
-import Proyek6 from "./assets/proyek/proyek6.webp";
-
 export const listProyek = [
   {
     id: 1,
     gambar: Proyek1,
     nama: "Website Sekolah",
+    image: "assets/proyek/proyek1.webp",
     desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
     tools: ["HTML", "CSS", "Javascript", "AOS"],
     dad: "200",
@@ -133,6 +127,7 @@ export const listProyek = [
     id: 2,
     gambar: Proyek2,
     nama: "Company Profile",
+    image: "assets/proyek/proyek2.webp",
     desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
     tools: ["HTML", "CSS", "Javascript", "AOS", "Swiper", "Lightbox Gallery"],
     dad: "300",
@@ -141,6 +136,7 @@ export const listProyek = [
     id: 3,
     gambar: Proyek3,
     nama: "Web Pernikahan 2.0",
+    image: "assets/proyek/proyek3.webp",
     desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
     tools: ["Vite", "ReactJS", "TailwindCSS", "AOS"],
     dad: "400",
@@ -149,6 +145,7 @@ export const listProyek = [
     id: 4,
     gambar: Proyek4,
     nama: "Website Course",
+    image: "assets/proyek/proyek4.webp",
     desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
     tools: ["Vite", "ReactJS", "Bootstrap", "AOS"],
     dad: "500",
@@ -157,6 +154,7 @@ export const listProyek = [
     id: 5,
     gambar: Proyek5,
     nama: "Web Portfolio",
+    image: "assets/proyek/proyek5.webp",
     desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
     tools: ["HTML", "CSS", "Javascript", "Bootsrap"],
     dad: "600",
@@ -165,6 +163,7 @@ export const listProyek = [
     id: 6,
     gambar: Proyek6,
     nama: "Company Profile 2.0",
+    image: "assets/proyek/proyek6.webp",
     desk: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis, laborum!",
     tools: ["NextJS", "TailwindCSS", "Framermotion"],
     dad: "700",
