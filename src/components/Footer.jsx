b9295874ca860495d390d41e65703e73cb15725c
IndraDevlop <indrawansyah2109@gmail.com>
2025-12-28 07:32:43 +0700

    fresh start

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='mt-25 py-4 flex md:flex-row flex-col gap-6 md:gap-0 justify-between items-center'>
+      <h1 className='text-[11px] font-bold'>©️ 2025 Designed By Indra</h1>
+      <div className='flex items-center gap-4 mr-5'>
+        <a href="https://github.com/IndraDevlop">
+          <i className='ri-github-fill ri-1x'></i>
+        </a>
+        <a href="https://www.instagram.com/indrawansyah_21/">
+          <i className='ri-instagram-fill ri-1x'></i>
+        </a>
+        <a href="https://facebook.com/indhra.onesyah">
+          <i className='ri-facebook-fill ri-1x'></i>
+        </a>
+        <a href="https://www.youtube.com/channel/UCmGChed0Yi4h9gRM_5NGqtw">
+          <i className='ri-youtube-fill ri-1x'></i>
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
